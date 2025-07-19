5da540cab167b1f696afa55d155b10fb98da9fee
ankz1991 <ankz1991@gmail.com>
2025-07-19 14:47:12 +0530

    first Python Assignment

---
+++ Assignment1/Task1_PerformBasicMathematicalOperations.py
@@ -0,0 +1,10 @@
+firstNumber = int(input("Enter the First Number : "))
+secondNumber = int(input("Enter the Second Number : "))
+addition = firstNumber + secondNumber
+substraction = firstNumber - secondNumber
+multiplication = firstNumber * secondNumber
+division = firstNumber/secondNumber
+print("Addition : ", addition)
+print("Substraction : ", substraction)
+print("Multiplication : ", multiplication)
+print("Division : ", division)
---
+++ Assignment1/Task2_Create_a_Personalized_Greeting.py
@@ -0,0 +1,5 @@
+FirstName = input("Enter First Name : ")
+LastName = input("Enter Last Name : ")
+blue = "\033[94m"
+reset = "\033[0m"
+print("Hello,"+FirstName + " " + LastName+"! Welcome "+blue+"to " +reset+"the Python Program")
